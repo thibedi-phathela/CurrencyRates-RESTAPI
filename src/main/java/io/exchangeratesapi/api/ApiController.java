b583f7ba7042ae6be05d7146d338282a027f6f01
thibedi-phathela <3932776@myuwc.ac.za>
2023-06-28 17:28:48 +0200

    Update

--- src/main/java/io/exchangeratesapi/api/ApiController.java
+++ src/main/java/io/exchangeratesapi/api/ApiController.java
@@ -1,6 +1,7 @@
 package io.exchangeratesapi.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 	
 	@Autowired
 	private ApiService apiService;
 	
-	@RequestMapping("/rates/{base}")
+	@GetMapping("/rates/{base}")
 	public Mono<String> getAllRates(@PathVariable String base){
 		return apiService.getAllRates(base);		
 	}
 	
-	@RequestMapping("/rate/{base}/{target}")
+	@GetMapping("/rate/{base}/{target}")
 	public Mono<String> getRate(@PathVariable String target, @PathVariable String base){
 		return apiService.getRate(base,target);
 	}
